3bf906b7f6bba830c256372d33ac6602fbb0f435
Epic Martijn <martijn.meeldijk@student.ucll.be>
2020-06-10 14:56:18 +0200

    het werkt yey

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+var wol = require('wake_on_lan');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {server:"Turn on Server"});
+});
+
+app.get('/on', function (req, res) {
+    wol.wake('D8CB8A3B5820', function(error) {
+        if (error) {
+            res.render('index', {server:"Error turning on Server"});
+        } else {
+            res.render('index', {server:"Turning on Server"});
+        }
+      });
+    
+  });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
