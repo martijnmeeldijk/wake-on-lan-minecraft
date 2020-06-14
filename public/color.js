5a8a6b89f8482f1a0d81472fa2a0158aab545d4b
Epic Martijn <martijn.meeldijk@student.ucll.be>
2020-06-14 13:50:25 +0200

    eaf

--- public/color.js
+++ public/color.js
@@ -1,1 +1,129 @@
-function obfuscate(e,o){function t(e,o){var t=0,n=o||e.innerHTML,a=n.length;obfuscators.push(window.setInterval(function(){t>=a&&(t=0),n=r(n,t),e.innerHTML=n,t++},0))}function n(e,o){return Math.floor(Math.random()*(o-e+1))+e}function r(e,o){var t=String.fromCharCode(n(64,90));return e.substr(0,o)+t+e.substr(o+1,e.length)}var a,c,f=o.childNodes.length;if(e.indexOf("<br>")>-1){o.innerHTML=e;for(var i=0;f>i;i++)c=o.childNodes[i],3===c.nodeType&&(a=document.createElement("span"),a.innerHTML=c.nodeValue,o.replaceChild(a,c),t(a))}else t(o,e)}function applyCode(e,o){for(var t=o.length,n=document.createElement("span"),r=!1,a=0;t>a;a++)n.style.cssText+=styleMap[o[a]]+";","§k"===o[a]&&(obfuscate(e,n),r=!0);return r||(n.innerHTML=e),n}function parseStyle(e){for(var o,t,n=e.match(/§.{1}/g)||[],r=[],a=[],c=document.createDocumentFragment(),f=n.length,e=e.replace(/\n|\\n/g,"<br>"),i=0;f>i;i++)r.push(e.indexOf(n[i])),e=e.replace(n[i],"\x00\x00");0!==r[0]&&c.appendChild(applyCode(e.substring(0,r[0]),[]));for(var i=0;f>i;i++){if(t=r[i+1]-r[i],2===t){for(;2===t;)a.push(n[i]),i++,t=r[i+1]-r[i];a.push(n[i])}else a.push(n[i]);a.lastIndexOf("§r")>-1&&(a=a.slice(a.lastIndexOf("§r")+1)),o=e.substring(r[i],r[i+1]),c.appendChild(applyCode(o,a))}return c}function clearObfuscators(){for(var e=obfuscators.length;e--;)clearInterval(obfuscators[e]);obfuscators=[]}function cutString(e,o,t){return e.substr(0,o)+e.substr(t+1)}var obfuscators=[],styleMap={"§4":"font-weight:normal;text-decoration:none;color:#be0000","§c":"font-weight:normal;text-decoration:none;color:#fe3f3f","§6":"font-weight:normal;text-decoration:none;color:#d9a334","§e":"font-weight:normal;text-decoration:none;color:#fefe3f","§2":"font-weight:normal;text-decoration:none;color:#00be00","§a":"font-weight:normal;text-decoration:none;color:#3ffe3f","§b":"font-weight:normal;text-decoration:none;color:#3ffefe","§3":"font-weight:normal;text-decoration:none;color:#00bebe","§1":"font-weight:normal;text-decoration:none;color:#0000be","§9":"font-weight:normal;text-decoration:none;color:#3f3ffe","§d":"font-weight:normal;text-decoration:none;color:#fe3ffe","§5":"font-weight:normal;text-decoration:none;color:#be00be","§f":"font-weight:normal;text-decoration:none;color:#ffffff","§7":"font-weight:normal;text-decoration:none;color:#bebebe","§8":"font-weight:normal;text-decoration:none;color:#3f3f3f","§0":"font-weight:normal;text-decoration:none;color:#000000","§l":"font-weight:bold","§n":"text-decoration:underline;text-decoration-skip:spaces","§o":"font-style:italic","§m":"text-decoration:line-through;text-decoration-skip:spaces"};String.prototype.replaceColorCodes=function(){clearObfuscators();var e=parseStyle(String(this));return e};
+var obfuscators = [];
+var styleMap = {
+    '§4': 'font-weight:normal;text-decoration:none;color:#be0000',
+    '§c': 'font-weight:normal;text-decoration:none;color:#fe3f3f',
+    '§6': 'font-weight:normal;text-decoration:none;color:#d9a334',
+    '§e': 'font-weight:normal;text-decoration:none;color:#fefe3f',
+    '§2': 'font-weight:normal;text-decoration:none;color:#00be00',
+    '§a': 'font-weight:normal;text-decoration:none;color:#3ffe3f',
+    '§b': 'font-weight:normal;text-decoration:none;color:#3ffefe',
+    '§3': 'font-weight:normal;text-decoration:none;color:#00bebe',
+    '§1': 'font-weight:normal;text-decoration:none;color:#0000be',
+    '§9': 'font-weight:normal;text-decoration:none;color:#3f3ffe',
+    '§d': 'font-weight:normal;text-decoration:none;color:#fe3ffe',
+    '§5': 'font-weight:normal;text-decoration:none;color:#be00be',
+    '§f': 'font-weight:normal;text-decoration:none;color:#ffffff',
+    '§7': 'font-weight:normal;text-decoration:none;color:#bebebe',
+    '§8': 'font-weight:normal;text-decoration:none;color:#3f3f3f',
+    '§0': 'font-weight:normal;text-decoration:none;color:#000000',
+    '§l': 'font-weight:bold',
+    '§n': 'text-decoration:underline;text-decoration-skip:spaces',
+    '§o': 'font-style:italic',
+    '§m': 'text-decoration:line-through;text-decoration-skip:spaces',
+};
+function obfuscate(string, elem) {
+    var magicSpan,
+        currNode,
+        len = elem.childNodes.length;
+    if(string.indexOf('<br>') > -1) {
+        elem.innerHTML = string;
+        for(var j = 0; j < len; j++) {
+            currNode = elem.childNodes[j];
+            if(currNode.nodeType === 3) {
+                magicSpan = document.createElement('span');
+                magicSpan.innerHTML = currNode.nodeValue;
+                elem.replaceChild(magicSpan, currNode);
+                init(magicSpan);
+            }
+        }
+    } else {
+        init(elem, string);
+    }
+    function init(el, str) {
+        var i = 0,
+            obsStr = str || el.innerHTML,
+            len = obsStr.length;
+        obfuscators.push( window.setInterval(function () {
+            if(i >= len) i = 0;
+            obsStr = replaceRand(obsStr, i);
+            el.innerHTML = obsStr;
+            i++;
+        }, 0) );
+    }
+    function randInt(min, max) {
+        return Math.floor( Math.random() * (max - min + 1) ) + min;
+    }
+    function replaceRand(string, i) {
+        var randChar = String.fromCharCode( randInt(64,90) ); /*Numbers: 48-57 Al:64-90*/
+        return string.substr(0, i) + randChar + string.substr(i + 1, string.length);
+    }
+}
+function applyCode(string, codes) {
+    var len = codes.length;
+    var elem = document.createElement('span'),
+        obfuscated = false;
+    for(var i = 0; i < len; i++) {
+        elem.style.cssText += styleMap[codes[i]] + ';';
+        if(codes[i] === '§k') {
+            obfuscate(string, elem);
+            obfuscated = true;
+        }
+    }
+    if(!obfuscated) elem.innerHTML = string;
+    return elem;
+}
+function parseStyle(string) {
+    var codes = string.match(/§.{1}/g) || [],
+        indexes = [],
+        apply = [],
+        tmpStr,
+        indexDelta,
+        noCode,
+        final = document.createDocumentFragment(),
+        len = codes.length,
+        string = string.replace(/\n|\\n/g, '<br>');
+    
+    for(var i = 0; i < len; i++) {
+        indexes.push( string.indexOf(codes[i]) );
+        string = string.replace(codes[i], '\x00\x00');
+    }
+    if(indexes[0] !== 0) {
+        final.appendChild( applyCode( string.substring(0, indexes[0]), [] ) );
+    }
+    for(var i = 0; i < len; i++) {
+    	indexDelta = indexes[i + 1] - indexes[i];
+        if(indexDelta === 2) {
+            while(indexDelta === 2) {
+                apply.push ( codes[i] );
+                i++;
+                indexDelta = indexes[i + 1] - indexes[i];
+            }
+            apply.push ( codes[i] );
+        } else {
+            apply.push( codes[i] );
+        }
+        if( apply.lastIndexOf('§r') > -1) {
+            apply = apply.slice( apply.lastIndexOf('§r') + 1 );
+        }
+        tmpStr = string.substring( indexes[i], indexes[i + 1] );
+        final.appendChild( applyCode(tmpStr, apply) );
+    }
+    return final;
+}
+function clearObfuscators() {
+    var i = obfuscators.length;
+    for(;i--;) {
+        clearInterval(obfuscators[i]);
+    }
+    obfuscators = [];
+}
+String.prototype.replaceColorCodes = function() {
+  clearObfuscators();
+  var outputString = parseStyle(String(this));
+  return outputString;
+};
+
+/////////////////////////////////////////////////
+function cutString(str, cutStart, cutEnd){
+  return str.substr(0,cutStart) + str.substr(cutEnd+1);
+}
