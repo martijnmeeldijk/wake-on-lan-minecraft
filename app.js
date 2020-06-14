8a1560edaf0af9c0f51f4b1a88d029b18cc80b5c
Epic Martijn <martijn.meeldijk@student.ucll.be>
2020-06-14 19:16:30 +0200

    outsourcing motd job

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.render('index', {server:"Turn on Server", time: ""});
 });
